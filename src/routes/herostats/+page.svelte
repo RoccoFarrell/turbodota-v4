9bc5a8483faaf9d803d5019cf3e821c570089573
Slippypeppy <97759240+Slippypeppy@users.noreply.github.com>
2023-12-05 17:22:44 -0500

    Updated table text colors

--- src/routes/herostats/+page.svelte
+++ src/routes/herostats/+page.svelte
@@ -272,10 +272,11 @@
 	function calculateWinPercentageClasses(win_percentage: number) {
 		//console.log(win_percentage)
 		let classes = '';
-		if (win_percentage < 0.45) classes = 'text-red-400';
-		if (win_percentage <= 0.4) classes = 'text-red-600';
-		if (win_percentage >= 0.55) classes = 'text-green-300';
-		if (win_percentage >= 0.6) classes = 'text-green-500';
+		if (win_percentage < 0.45) classes = 'text-red-800';
+		if (win_percentage <= 0.465) classes = 'text-red-600';
+		if (win_percentage <= 0.49) classes = 'text-red-400';
+		if (win_percentage >= 0.51) classes = 'text-green-300';
+		if (win_percentage >= 0.535) classes = 'text-green-500';
 
 		return classes;
 	}
@@ -283,10 +284,10 @@
 	function calculateKdaClasses(kda: number) {
 		//console.log(kda)
 		let classes = '';
-		if (kda < 4) classes = 'text-red-400';
-		if (kda <= 2) classes = 'text-red-600';
-		if (kda >= 5) classes = 'text-green-300';
-		if (kda >= 7) classes = 'text-green-500';
+		if (kda < 2.5) classes = 'text-red-600';
+		if (kda <= 3) classes = 'text-red-400';
+		if (kda >= 3.5) classes = 'text-green-300';
+		if (kda >= 4) classes = 'text-green-500';
 
 		return classes;
 	}
