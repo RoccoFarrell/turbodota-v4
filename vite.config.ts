929e9c567f788b54ae1b9def81da6a3f852e65c5
Rocco Farrell <farrell.roc@gmail.com>
2023-12-05 15:15:46 -0500

    Resolve @s during build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./node_modules")
+		}
+	}
 });
