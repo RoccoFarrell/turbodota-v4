380d248be626fac5c71861c39dc97829a89e5cf3
Rocco Farrell <farrell.roc@gmail.com>
2023-12-05 15:12:43 -0500

    Split deployment

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,13 +1,31 @@
-import { PrismaClient } from "@prisma/client"
+//import { PrismaClient } from "@prisma/client"
+import { PrismaClient as PrismaClientProd} from '@prisma-prod/client'
+import { PrismaClient as PrismaClientDev} from '@prisma-dev/client'
 import { env } from "$env/dynamic/private"
 
-const prisma = global.__prisma || new PrismaClient()
+
+//const prisma = global.__prisma || new PrismaClient()
 // const client = new PrismaClient({
 // 	log: ['query', 'info', 'warn', 'error']
 // })
 
-// if (env.NODE_ENV === "development") {
-// 	global.__prisma = client
-// }
+const prismaClientSingleton = () => {
+
+    console.log(`[prisma.ts] Running in ${process.env.NODE_ENV}`)
+    if (process.env.NODE_ENV !== "development") {
+        return new PrismaClientProd()     
+    } else return new PrismaClientDev()
+    
+}
+
+declare global {
+    var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export { prisma }
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+//export { prisma }
