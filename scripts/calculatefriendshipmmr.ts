72207d3b16d53939b3a70ccaab373eb109b36b44
Josh Martin <josh.martin819@gmail.com>
2023-12-26 13:06:45 -1000

    Continued mmr script. Needs to use win rates and post calculated values.

--- scripts/calculatefriendshipmmr.ts
+++ scripts/calculatefriendshipmmr.ts
@@ -5,15 +5,11 @@ function sleep(ms: number) {
 	return new Promise((resolve) => setInterval(resolve, ms));
 }
 
-//Get All Matches for player from TurboTown
-
-//TEMP
 const data = {
     accountIdsForCalc: [65110965, 34940151, 80636612, 113003047, 125251142, 423076846, 67762413, 68024789]
 };
 
-
-let winrates = await fetch('https://turbodota.com/api/winrates', {
+let winrates = await fetch('https://turbodota.com/api/winrates?source=db', {
 	method: 'POST',
 	headers: {
 		Accept: 'application/json',
@@ -27,28 +23,79 @@ let winrates = await fetch('https://turbodota.com/api/winrates', {
 	});
 
     console.log(winrates)
-async function getAllMatches(account_id: number) {
+
+
+async function getAllMatches(account_id: number, winrates) {
 	let player_match_response = await fetch('https://turbodota.com/api/updateMatchesForUser/' + account_id);
 	let player_match_response_object = await player_match_response.json();
 
-	console.log(player_match_response_object);
+	//console.log(player_match_response_object);
 	for (const match of player_match_response_object.matchData) {
+        const friendsArray = Array();
+        const friendsArray2 = Array();
+
+
 		let send_match_for_parse_response = await fetch('https://turbodota.com/api/matches/' + match.match_id);
 		let send_match_for_parse_response_object = await send_match_for_parse_response.json();
 
-		for (const player of send_match_for_parse_response_object.matchDetailResult.matchDetail.players) {
-			if (playersWeCareAbout.includes(player.account_id) && player.account_id != account_id) {
-				console.log('Account ID: ' + player.account_id + ' | Hero ID: ' + player.hero_id);
+        console.log("Now handling match ID: " + match.match_id)
 
-				
-			}
+		for (const player of send_match_for_parse_response_object.matchDetailResult.matchDetail.players) {
+            //console.log(player)
+			if (playersWeCareAbout.includes(player.account_id) && player.account_id != account_id) {
+                console.log("Found a friend to handle: " + player.account_id)
+                let tempArray = {}
+                tempArray.account_id = player.account_id
+                tempArray.hero_id = player.hero_id
+                if (player.win == 1) {tempArray.win_or_loss = 1}
+                else {tempArray.win_or_loss = 0}
+                friendsArray.push(tempArray)
+            }
 		}
+        console.log("Friends Array To Handle: " + JSON.stringify(friendsArray))
+
+        for (const item of friendsArray) {
+            let tempArray = {}
+            tempArray.account_id = item.account_id
+            tempArray.hero_id = item.hero_id
+            tempArray.win_or_loss = item.win_or_loss
+            tempArray.win_rate = .4 // actually get win rates
+            friendsArray2.push(tempArray)
+        }
+        console.log("Friends Array 2 To Handle: " + JSON.stringify(friendsArray2))
+
+        //Do the math
+        let mmrIndividualModifier = 0
+        let mmrTotalMatchModifier = 0
+        let i = 0
+        for (const item of friendsArray2) {
+            if (item.win_loss_flag == 1){
+                mmrIndividualModifier = 5 + ((item.win_rate-.5)*10*-1)
+                console.log("Won match! Adding to individual modifier: " + mmrIndividualModifier)
+            }
+            else {
+                mmrIndividualModifier = 5 - ((item.win_rate-.5)*10*-1)
+                console.log("Lost match! Subtracting from individual modifier: " + mmrIndividualModifier)
+            }
+            mmrTotalMatchModifier += mmrIndividualModifier
+            console.log("mmrTotalMatchModifier is now: " + mmrTotalMatchModifier)
+            i++
+        }
+        console.log("mmrTotalMatchModifier is this before adding missing players: " + mmrTotalMatchModifier)
+        mmrTotalMatchModifier = mmrTotalMatchModifier + (5*(5-i))
+        console.log("mmrTotalMatchModifier is this after adding missing players: " + mmrTotalMatchModifier)
+
+        //Store the mmrTotalMatchModifier value properly
+
+
+        console.log("Sleeping till next match")
+        await sleep(30000); //Can make this lower once it works, just used this for testing a single match lazily
 	}
-	await sleep(3000);
+	
 }
 
 let playerID = 80636612;
-getAllMatches(playerID);
+getAllMatches(playerID, winrates);
 
 // 80636612   - Martin
 // 34940151   - Roberts
