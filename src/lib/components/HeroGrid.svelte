a4c3da8130b52226e51f594fbb387a6388585e88
Slippypeppy <97759240+Slippypeppy@users.noreply.github.com>
2023-12-31 16:17:31 -0500

    updates to snake draft

--- src/lib/components/HeroGrid.svelte
+++ src/lib/components/HeroGrid.svelte
@@ -2,29 +2,44 @@
 	//page data
 
 	export let allHeroes: Hero[] = [];
+	import { createEventDispatcher } from 'svelte';
+	//stores
+	import { randomStore } from '$lib/stores/randomStore';
 
-	import { calculateKdaClasses } from '$lib/helpers/tableColors';
-	import daysAgoString from '$lib/helpers/daysAgo';
-    import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
 
-    const dispatch = createEventDispatcher();
-
-    
-    const test = (hero: Hero) => {
-       dispatch('heroclick', hero);
-       //console.log(hero)
+	const heroClick = (hero: Hero) => {
+		dispatch('heroclick', hero);
+		showHero = false;
 	};
-    
+	let showHeroGrid = true;
+	let showHero=true;
 </script>
 
 <!-- Desktop Hero Grid -->
 <div
 	id="desktopHeroGrid"
-	class={`z-0 flex flex-wrap max-w-[95%] p-4 max-md:hidden xs:visible justify-center overflow-y-auto w-full max-h-[50rem] ${'visible border border-dashed border-red-500'}`}
+	class={`z-0 flex flex-wrap max-w-[95%] p-4 max-md:hidden xs:visible justify-center overflow-y-auto w-full max-h-[50rem] ${
+		showHeroGrid ? 'visible border border-dashed border-red-500' : 'border-double border-t-4 border-amber-500'
+	}`}
 >
 	{#each allHeroes as hero}
 		<div class="object-contain m-1 relative">
-			<button on:click={() => test(hero)}><i class={`z-0 d2mh hero-${hero.id}`}></i></button>
+			<button on:click={() => heroClick(hero)}><i class={`z-0 d2mh hero-${hero.id}`}></i></button>
+		</div>
+	{/each}
+</div>
+
+<!-- Mobile Hero Grid -->
+<div
+	id="mobileHeroGrid"
+	class={`w-full flex flex-wrap max-w-[95%] p-2 md:hidden max-md:visible justify-center overflow-y-auto max-h-96 ${
+		showHeroGrid ? 'visible border border-dashed border-red-500' : 'border-double border-b-4 border-amber-500'
+	}`}
+>
+	{#each allHeroes as hero}
+		<div class="w-full h-full bg-red-600 rounded-xl z-10 absolute bg-opacity-70">
+			<button on:click={() => heroClick(hero)}><i class={`z-0 d2mh hero-${hero.id}`}></i></button>
 		</div>
 	{/each}
 </div>
--- src/routes/snakedraft/+page.svelte
+++ src/routes/snakedraft/+page.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-
+	import type { League } from '@prisma/client';
 	export let data: PageData;
-    import HeroGrid from '$lib/components/HeroGrid.svelte';
+	import HeroGrid from '$lib/components/HeroGrid.svelte';
+
+	//console.log(data.leagues);
 
 	const onNext = (hero: Hero) => {
 		console.log(hero);
 	};
+
+	data.leagues.forEach((element) => {
+		console.log(element.members);
+	});
 </script>
 
-<div><div class="my-4"><HeroGrid on:heroclick={onNext} allHeroes={data.heroDescriptions.allHeroes} /></div></div>
+<div class="container md:m-4 my-4 h-full mx-auto w-full max-sm:mb-20">
+	<div>
+		<div class="my-4"><HeroGrid on:heroclick={onNext} allHeroes={data.heroDescriptions.allHeroes} /></div>
+	</div>
+	
+	{#each data.leagues[0].members as member}
+		<div class="mb-2 bg-surface-500/10 p-4 w-4/5 mx-auto shadow-md">
+			<h3 class="h3 dark:text-yellow-500 text-primary-500">{member.account_id}</h3>
+			<p class="text-xs">Next turn in: 4 picks</p>
+		</div>
+	{/each}
+</div>
