584c153f74a0c7b5a92203182af7497f9dfd6b02
Slippypeppy <97759240+Slippypeppy@users.noreply.github.com>
2024-01-09 23:55:19 -0500

    Added more features to shop

--- src/lib/components/Shop.svelte
+++ src/lib/components/Shop.svelte
@@ -4,6 +4,16 @@
 	import type { PopupSettings } from '@skeletonlabs/skeleton';
 	import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
 	import type { TableSource } from '@skeletonlabs/skeleton';
+	import { Modal, getModalStore } from '@skeletonlabs/skeleton';
+	import type { ModalSettings, ModalComponent, ModalStore } from '@skeletonlabs/skeleton';
+
+	const modalStore = getModalStore();
+	const modal: ModalSettings = {
+		type: 'alert',
+		// Data
+		title: 'Purchase Confirmed',
+		body: 'Thank you for purchasing, come again!'
+	};
 
 	class ShopItem {
 		id: number = -1;
@@ -11,11 +21,16 @@
 		description: string = '';
 		imgSrc: string = '';
 		goldCost: number = 0;
-		quantity: number = 0;
+		quantityAvailable: number = 0;
 	}
 
+	class ShoppingCart {
+		itemList: Array<ShopItem> = [];
+		totalCost: number = 0;
+	}
+
+	let userShoppingCart: ShoppingCart = new ShoppingCart();
 	let availableItems: Array<ShopItem> = [];
-	let selectedItems: Array<ShopItem> = [];
 
 	let lotusOrb: ShopItem = new ShopItem();
 	lotusOrb = {
@@ -24,7 +39,7 @@
 		description: 'This item will reflect back any attempted debuff applied to you',
 		imgSrc: 'https://cdn.dota2.com/apps/dota2/images/items/lotus_orb_lg.png',
 		goldCost: 500,
-		quantity: 0
+		quantityAvailable: 10
 	};
 
 	let linkensSphere: ShopItem = new ShopItem();
@@ -34,7 +49,7 @@
 		description: 'This item will block any attempted debuff applied to you',
 		imgSrc: 'https://cdn.dota2.com/apps/dota2/images/items/sphere_lg.png',
 		goldCost: 1000,
-		quantity: 0
+		quantityAvailable: 5
 	};
 
 	let observerWard: ShopItem = new ShopItem();
@@ -44,7 +59,7 @@
 		description: 'This item will let you pick from a selection of 3 random heroes',
 		imgSrc: 'https://cdn.dota2.com/apps/dota2/images/items/ward_observer_lg.png',
 		goldCost: 100,
-		quantity: 0
+		quantityAvailable: 100
 	};
 
 	availableItems.push(lotusOrb);
@@ -53,18 +68,16 @@
 
 	const tableSource: TableSource = {
 		// A list of heading labels.
-		head: ['Item Name', 'Gold', 'Quantity'],
+		head: ['Item Name', 'Gold', 'Quantity Available', 'Quantity'],
 		// The data visibly shown in your table body UI.
-		body: tableMapperValues(availableItems, ['name', 'goldCost', 'quantity']),
+		body: tableMapperValues(availableItems, ['name', 'goldCost', 'quantityAvailable']),
 		// Optional: The data returned when interactive is enabled and a row is clicked.
-		meta: tableMapperValues(availableItems, ['id', 'name', 'description', 'imgSrc', 'goldCost', 'quantity'])
+		meta: tableMapperValues(availableItems, ['id', 'name', 'description', 'imgSrc', 'goldCost', 'quantityAvailable'])
 		// Optional: A list of footer labels.
 		//foot: ['Total', '', '<code class="code">5</code>']
 	};
 
-	console.log(availableItems);
-
-	let hoverItem = new ShopItem();
+	let selectedDetailItem = new ShopItem();
 	//let selectedItem = new ShopItem();
 
 	// const hoverHandler = (inputItem: ShopItem) => {
@@ -76,34 +89,62 @@
 	// 	//console.log(activeItem);
 	// };
 
-	const clickHandler = (inputItem: ShopItem) => {
-		console.log(selectedItems);
-		if (selectedItems.filter((item: ShopItem) => item.id == inputItem.id).length > 0) {
-			console.log('item detected');
-		} else {
-			console.log('item added');
-			selectedItems.push(inputItem);
+	// const clickHandler = (inputItem: ShopItem) => {
+	// 	console.log(selectedItems);
+	// 	if (selectedItems.filter((item: ShopItem) => item.id == inputItem.id).length > 0) {
+	// 		console.log('item detected');
+	// 	} else {
+	// 		console.log('item added');
+	// 		selectedItems.push(inputItem);
+	// 	}
+	// 	console.log(selectedItems);
+	// };
+    
+	const quantityMinusClickHandler = (inputTableSource: TableSource, inputRow: number) => {
+		//let itemSubstract = availableItems.filter((item: ShopItem) => item.id == Number(inputTableSource.meta[inputRow][0]))
+		const findIndex = userShoppingCart.itemList.findIndex(
+			(item) => item.id === Number(inputTableSource.meta[inputRow][0])
+		);
+		let goldCost = Number(inputTableSource.meta[inputRow][4]);
+
+		if (findIndex !== -1) {
+			userShoppingCart.itemList.splice(findIndex, 1);
+			userShoppingCart.totalCost -= goldCost;
 		}
-		console.log(selectedItems);
+
+		//console.log('shopping cart', userShoppingCart.itemList);
+		//console.log('total cost', userShoppingCart.totalCost);
 	};
 
-    const quantityPlusClickHandler = () => {
-        
-    }
+	const quantityPlusClickHandler = (inputTableSource: TableSource, inputRow: number) => {
+		const findIndex = availableItems.findIndex((item) => item.id === Number(inputTableSource.meta[inputRow][0]));
+		let itemAdd = availableItems.filter((item: ShopItem) => item.id == Number(inputTableSource.meta[inputRow][0]));
+		let goldCost = Number(inputTableSource.meta[inputRow][4]);
 
-    const quantityMinusClickHandler = () => {
-        
-    }
+		if (
+			itemAdd[0].quantityAvailable >
+			userShoppingCart.itemList.filter((item: ShopItem) => item.id == Number(inputTableSource.meta[inputRow][0])).length
+		) {
+			userShoppingCart.itemList.push(itemAdd[0]);
+			userShoppingCart.totalCost += goldCost;
+		}
 
-    const rowClickHandler = (inputTableSource: TableSource, inputRow: number) => {
-        console.log(inputTableSource.meta[inputRow])
-        hoverItem.id = inputTableSource.meta[inputRow][0];
-		hoverItem.name = inputTableSource.meta[inputRow][1];
-		hoverItem.description = inputTableSource.meta[inputRow][2];
-		hoverItem.goldCost = inputTableSource.meta[inputRow][4];
-    }
+		//console.log('shopping cart', userShoppingCart.itemList);
+		//console.log('total cost', userShoppingCart.totalCost);
+	};
 
-	console.log(availableItems.filter((item: any) => item.name == 'Lotus Orb')[0].imgSrc);
+	const rowClickHandler = (inputTableSource: TableSource, inputRow: number) => {
+		selectedDetailItem.id = Number(inputTableSource.meta[inputRow][0]);
+		selectedDetailItem.name = inputTableSource.meta[inputRow][1];
+		selectedDetailItem.description = inputTableSource.meta[inputRow][2];
+		selectedDetailItem.goldCost = Number(inputTableSource.meta[inputRow][4]);
+	};
+
+	const purchaseClickHandler = () => {
+		modalStore.trigger(modal);
+		userShoppingCart = new ShoppingCart();
+	};
+
 </script>
 
 <div
@@ -116,14 +157,13 @@
 	<div
 		class="md:w-full max-md:max-w-sm text-center h-fit items-center dark:bg-surface-600/30 bg-surface-200/30 border border-surface-200 dark:border-surface-700 shadow-lg rounded-xl px-2 md:py-2 max-sm:py-2"
 	>
-		<!-- Auto Bans -->
 		<div>
 			<div class="mb-2 bg-surface-500/10 p-4 rounded-full w-4/5 mx-auto shadow-md">
 				<h3 class="h3 dark:text-yellow-500 text-primary-500">Items</h3>
 			</div>
 		</div>
 
-        <!-- <Table
+		<!-- <Table
 			interactive={true}
 			source={tableSource}
 			on:selected={}
@@ -144,19 +184,38 @@
 						<tr on:click={() => rowClickHandler(tableSource, i)}>
 							<td><img class="h-auto max-w-full rounded-lg" src={availableItems[i].imgSrc} alt="" />{row[0]}</td>
 							<td>{row[1]}</td>
+							<td>{row[2]}</td>
 							<td
-								><button class="btn-icon" on:click={quantityPlusClickHandler}> <i class="fi fi-sr-square-minus" /></button>
-								{row[2]} <button class="btn-icon" on:click={quantityMinusClickHandler}> <i class="fi fi-sr-square-plus" /></button></td
+								><button class="btn-icon" on:click={() => quantityMinusClickHandler(tableSource, i)}>
+									<i class="fi fi-sr-square-minus" /></button
+								>
+								{userShoppingCart.itemList.filter((item) => item.id == Number(tableSource.meta[i][0])).length}
+								<button class="btn-icon" on:click={() => quantityPlusClickHandler(tableSource, i)}>
+									<i class="fi fi-sr-square-plus" /></button
+								></td
 							>
 						</tr>
 					{/each}
 				</tbody>
 				<tfoot>
 					<tr>
-						<td>Total</td>
+						<td>Total {userShoppingCart.totalCost}</td>
 					</tr>
 				</tfoot>
 			</table>
+			{#if userShoppingCart.itemList.length > 0}
+				<button
+					disabled={false}
+					class="btn variant-filled-primary w-full my-16 max-lg:fixed max-lg:bottom-0 max-lg:left-0 max-lg:my-8 max-lg:mx-4 max-lg:max-w-[90%] md:max-w-[80%]"
+					on:click={() => purchaseClickHandler()}>Purchase</button
+				>
+			{:else}
+				<button
+					disabled={true}
+					class="btn variant-filled-primary w-full my-16 max-lg:fixed max-lg:bottom-0 max-lg:left-0 max-lg:my-8 max-lg:mx-4 max-lg:max-w-[90%] md:max-w-[80%]"
+					>Purchase</button
+				>
+			{/if}
 		</div>
 	</div>
 </div>
@@ -165,19 +224,20 @@
 	<div
 		class="md:w-full max-md:max-w-sm text-center h-fit items-center dark:bg-surface-600/30 bg-surface-200/30 border border-surface-200 dark:border-surface-700 shadow-lg rounded-xl px-2 md:py-2 max-sm:py-2"
 	>
-		<!-- Auto Bans -->
 		<div class="group">
 			<div class="mb-2 bg-surface-500/10 p-4 rounded-full w-4/5 mx-auto shadow-md">
 				<h3 class="h3 dark:text-yellow-500 text-primary-500">Item Details</h3>
 			</div>
 			<!-- {#if activeItem.id !== -1} -->
-			<p class="text-m text-left">Item: {hoverItem.name}</p>
-			<p class="text-m text-left">Description: {hoverItem.description}</p>
-			<p class="text-m text-left dark:text-yellow-500">Gold Cost: {hoverItem.goldCost}</p>
+			<p class="text-m text-left">Item: {selectedDetailItem.name}</p>
+			<p class="text-m text-left">Description: {selectedDetailItem.description}</p>
+			<p class="text-m text-left dark:text-yellow-500">Gold Cost: {selectedDetailItem.goldCost}</p>
 			<!-- {/if} -->
 		</div>
 	</div>
 </div>
+
+<Modal />
 
 <!-- <section class="flex justify-center gap-8">
     <div>
