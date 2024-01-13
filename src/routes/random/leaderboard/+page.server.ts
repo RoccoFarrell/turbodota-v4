45ae225548680f6a0c1b432beba556d0a8d73fcb
Rocco Farrell <farrell.roc@gmail.com>
2024-01-13 00:25:57 -0500

    Fixed season 2 leaderboard

--- src/routes/random/leaderboard/+page.server.ts
+++ src/routes/random/leaderboard/+page.server.ts
@@ -4,6 +4,15 @@ import prisma from '$lib/server/prisma';
 export const load: PageServerLoad = async ({ locals, parent, url }) => {
     const parentData = await parent();
     let response = await prisma.random.findMany({
+        where: {
+            seasons: {
+                every: {
+                    id: {
+                        equals: 2
+                    }
+                }
+            }
+        },
         include: {
             user: true,
             match: true
