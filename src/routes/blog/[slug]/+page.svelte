b35a7075c1aba62d1562d59615cd70e9337972e2
Rocco Farrell <farrell.roc@gmail.com>
2024-01-19 11:36:31 -0500

    Improved blog page

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -12,11 +12,25 @@
 </svelte:head>
 
 <div class="w-full flex justify-center">
-	<article class="p-8">
+	<div class="w-64 xl:h-full fixed max-lg:bottom-0 lg:left-64 max-lg:hidden">
+		<div class="h-full border-r border-dashed border-orange-500/30 flex flex-col items-center p-2 space-y-2">
+			<h4 class="h4 text-amber-500">Blog Posts</h4>
+			{#each data.posts as post}
+				<div class="card p-4">
+					<a href={`/blog/${post.slug}`} class="h5 text-secondary-500">{post.title}</a>
+					<p class="italic text-tertiary-400 text-xs">{post.description}</p>
+					<p class="date text-xs">{formatDate(post.date)}</p>
+					
+				</div>
+		{/each}
+
+		</div>
+	</div>
+	<article class="p-8 lg:min-w-[50%] lg:ml-64">
 		<div class="container flex flex-col">
 			<!-- Title -->
 			<hgroup class="mb-8">
-				<h1 class="h1 font-bold">{data.meta.title}</h1>
+				<h1 class="h1 font-bold text-center">{data.meta.title}</h1>
 				<h4 class="h4 text-amber-500 text-center">{data.meta.description}</h4>
 			</hgroup>
 
--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -2,11 +2,16 @@ import { error } from '@sveltejs/kit'
 
 export async function load({ params }) {
 	try {
+
+		const response = await fetch('/api/posts')
+		const posts: Post[] = await response.json()
+
 		const post = await import(`../../../posts/${params.slug}.md`)
 
 		return {
 			content: post.default,
-			meta: post.metadata
+			meta: post.metadata,
+			posts
 		}
 	} catch (e) {
 		throw error(404, `Could not find ${params.slug}`)
