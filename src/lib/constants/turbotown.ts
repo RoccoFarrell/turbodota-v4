00f862869698ddc323dfa197789443d6cbc4f3a7
Rocco Farrell <farrell.roc@gmail.com>
2024-01-19 11:54:01 -0500

    4.10c

--- src/lib/constants/turbotown.ts
+++ src/lib/constants/turbotown.ts
@@ -2,6 +2,6 @@ export const constant_townVersion: string = "4.10b"
 export const constant_questXP: number = 100
 export const constant_questGold: number = 100
 export const questWin_xpMultiplier: number = 1.5
-export const questLoss_xpMultiplier: number = .75
+export const questLoss_xpMultiplier: number = .5
 export const questWin_goldMultiplier: number = 1
 export const questLoss_goldMultiplier: number = 0
